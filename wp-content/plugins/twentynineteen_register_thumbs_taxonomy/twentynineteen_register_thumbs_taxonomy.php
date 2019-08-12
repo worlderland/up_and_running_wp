c85272972e00b20a065586fa2fba1383b50137ea
Alessandro Coraglia <info.alessandrocoraglia@gmail.com>
2019-08-12 09:25:53 +0800

    End of page 255 of the book - Found issues with code

--- wp-content/plugins/twentynineteen_register_thumbs_taxonomy/twentynineteen_register_thumbs_taxonomy.php
+++ wp-content/plugins/twentynineteen_register_thumbs_taxonomy/twentynineteen_register_thumbs_taxonomy.php
@@ -1,39 +1,20 @@
 <?php
 // Plugin Name: Twentynineteen Register Thumbs Taxonomy
 
-// function twentynineteen_register_thumbs_taxonomy() {
-//     $args = array(
-//         'hierarchical' => true,
-//         'label' => 'Thumbs Up/Down',
-//     );
-//     register_taxonomy('thumbs', 'post', $args);
-// }
-// add_action('init', 'twentynineteen_register_thumbs_taxonomy');
-
-// function twentynineteen_register_thumbs_terms() {
-//     wp_insert_term('Thumbs Up!', 'thumbs', $args = array(
-//         'description' => 'This thing gets a thumbs up!'
-//     ));
-//     wp_insert_term('Thumbs Down!', 'thumbs', $args = array(
-//         'description' => 'This thing gets a thumbs down!'
-//     ));
-// }
-// add_action('init', 'twentynineteen_register_thumbs_terms');
-
 function twentynineteen_register_thumbs_taxonomy() {
         $args = array(
         'hierarchical' => true,
         'label' => 'Thumbs Up/Down',
         );
-        register_taxonomy( 'tthhuummbbss', 'post', $args );
+        register_taxonomy( 'thumbs', 'post', $args );
     }
     add_action( 'init', 'twentynineteen_register_thumbs_taxonomy' );
 
 function twentynineteen_register_thumbs_terms() {
-    wp_insert_term( 'Thumbs Up!', 'thumbs' ,$args =array(
+    wp_insert_term( 'Thumbs Up!', 'thumbs' ,$args = array(
         'description' => 'This thing gets a Thumbs Up!'
     ));
-    wp_insert_term( 'Thumbs Down!', 'thumbs' ,$args =array(
+    wp_insert_term( 'Thumbs Down!', 'thumbs' ,$args = array(
         'description' => 'This thing gets a Thumbs down!'
     ));
     }

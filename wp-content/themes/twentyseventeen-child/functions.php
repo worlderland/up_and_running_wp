007d73b7f09ef9ef95040d203e9dfbc7847dc936
Alessandro Coraglia <info.alessandrocoraglia@gmail.com>
2019-08-19 15:00:31 +0800

    Currently on Page 298 of the book

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -14,3 +14,19 @@ function twentyseventeen_filter_example($title)
     return 'Hooked by Child Theme: ' . $title;
 }
 add_filter('the_title', 'twentyseventeen_filter_example');
+
+//Creating a widgets area
+function twentynineteen_theme_slug_widgets_init() {
+    $args = array(
+        'name' => 'Widgetised sidebar',
+        'id' => 'widgetised-sidebar',
+        'description' => 'Our Widgetised Sidebar',
+        'class' => '',
+        'before_widget' => '<li id="%1$s" class="%2$s">',
+        'after_widget' => '</li>',
+        'before_title' => '<h2  class="widgettitle">',
+        'after_title' => '</h2>',
+    );
+    register_sidebar($args);
+}
+add_action('widgets_init', 'twentynineteen_theme_slug_widgets_init');
